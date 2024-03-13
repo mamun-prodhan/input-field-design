0150715ee585c55d01ad828d8176e4af3b041ae4
mamun-prodhan <mamunpr9@gmail.com>
2024-03-13 08:09:50 +0600

    radio added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="min-w-screen min-h-screen bg-white flex flex-col items-center justify-center">
-      <div>
+      {/* <div>
         <label className="block mb-2" htmlFor="text">
           Enter Your Name Here
         </label>
@@ -23,9 +23,45 @@ function App() {
           id="text"
           placeholder="Your Name"
         />
+      </div> */}
+
+      {/* custom radio */}
+      <div className="max-w-6xl mx-auto px-12">
+        <div className="flex flex-wrap gap-3">
+          <label htmlFor="" className="cursor-pointer">
+            <input type="radio" className="peer sr-only" name="pricing" />
+            <div className="w-72 max-w-xl p-5 bg-white text-gray-600 rounded-md hover:shadow ring-2 ring-transparent peer-checked:text-sky-600 peer-checked:ring-sky-500 peer-checked:ring-offset-2">
+              <div className="flex flex-col gap-1">
+                <div className="flex items-center justify-between">
+                  <p className="text-sm font-semibold uppercase text-gray-500">
+                    essential
+                  </p>
+                  <div>
+                    <svg
+                      viewBox="0 0 1024 1024"
+                      fill="currentColor"
+                      height="24"
+                      width="24"
+                    >
+                      <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 01-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z" />
+                    </svg>
+                  </div>
+                </div>
+                <div className="flex items-end justify-between">
+                  <p>
+                    <span className="text-lg font-bold">12 GB</span> of storage
+                  </p>
+                  <p className="text-sm font-bold">$12 / mon</p>
+                </div>
+              </div>
+            </div>
+          </label>
+        </div>
       </div>
     </div>
   );
 }
 
 export default App;
+
+// https://play.tailwindcss.com/f6zsuCPdwj
